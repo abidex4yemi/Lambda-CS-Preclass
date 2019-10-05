b75058de6b5fed1357a0267fb4b171967b48b8b0
abidex4yemi <abidex4yemi.com>
2019-10-05 17:38:19 +0100

    Create basic binary search

--- binary_and_linear_search.py
+++ binary_and_linear_search.py
@@ -4,6 +4,9 @@
 # target
 
 # Basic binary search
+# O(n) because the operational time is
+# proportional to the input size n
+import math
 
 
 def linear_search(arr, target):
@@ -15,4 +18,28 @@ def linear_search(arr, target):
 
 
 numbers = list(range(0, 50, 3))
-print(linear_search(numbers, 30))
+# print(linear_search(numbers, 30))
+
+# Note precedence of operators count
+# in expression evaluation
+
+# O(logn) => Logarithmic Time
+
+
+def binary_search(arr, target):
+    low = 0
+    high = len(arr) - 1
+
+    while low <= high:
+        middle = int(math.floor((low + high) / 2))
+        if target < arr[middle]:
+            high = middle - 1
+        elif target > arr[middle]:
+            low = middle + 1
+        else:
+            return middle
+
+    return -1
+
+
+print(binary_search([20, 30, 40, 50, 60], 50))
